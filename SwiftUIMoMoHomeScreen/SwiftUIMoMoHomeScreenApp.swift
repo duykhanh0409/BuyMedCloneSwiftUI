d4bd0603af787a82e32a4df98a0ec68cf435510c
KhanhNguyen4999 <khanh.nguyenduy@buymed.asia>
2023-03-04 11:25:39 +0700

    Initial Commit

---
+++ SwiftUIMoMoHomeScreen/SwiftUIMoMoHomeScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIMoMoHomeScreenApp.swift
+//  SwiftUIMoMoHomeScreen
+//
+//  Created by Thuocsi on 04/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIMoMoHomeScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
