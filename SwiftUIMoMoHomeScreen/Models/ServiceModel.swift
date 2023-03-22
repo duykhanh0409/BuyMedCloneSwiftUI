0d487a3343ac08973997ed995b718a94f5438a84
KhanhNguyen4999 <khanh.nguyenduy@buymed.asia>
2023-03-22 17:40:20 +0700

    feat: create Service model

---
+++ SwiftUIMoMoHomeScreen/Models/ServiceModel.swift
@@ -0,0 +1,41 @@
+//
+//  ServiceModel.swift
+//  SwiftUIMoMoHomeScreen
+//
+//  Created by Thuocsi on 22/03/2023.
+//
+
+import Foundation
+
+// MARK: - Welcome
+struct Welcome: Codable {
+    let status: String
+    let data: [Datum]
+    let message: String
+}
+
+// MARK: - Datum
+struct Datum: Codable {
+    let appCode, appValueCode, createdTime: String
+    let lastUpdatedTime: String?
+    let value: Value
+}
+
+// MARK: - Value
+struct Value: Codable {
+    let defaultParam, description, extra, icon: String
+    let key, language, name, port: String
+    let query, refID: String
+    let sortOrder: Int
+    let special: String
+    let status: Int
+    let uri, user: String
+
+    enum CodingKeys: String, CodingKey {
+        case defaultParam, description, extra, icon, key, language, name, port, query
+        case refID = "refId"
+        case sortOrder, special, status, uri, user
+    }
+}
+
+
