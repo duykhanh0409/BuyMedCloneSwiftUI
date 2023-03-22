9a645b95d57d2757671cb4473a26d470a1e44154
KhanhNguyen4999 <khanh.nguyenduy@buymed.asia>
2023-03-22 17:52:32 +0700

    feat: create apiService for call api from url

---
+++ SwiftUIMoMoHomeScreen/Services/ApiService.swift
@@ -0,0 +1,34 @@
+//
+//  ApiService.swift
+//  SwiftUIMoMoHomeScreen
+//
+//  Created by Thuocsi on 22/03/2023.
+//
+
+import Foundation
+
+class ApiService{
+    static func fetchData <T:Codable> (urlString: String, type: T.Type, method: String, onSuccess: @escaping (T)->Void, onFail: @escaping(String) -> Void) {
+        
+        guard let url = URL(string: urlString) else {return}
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = method
+        
+        
+        let task = URLSession.shared.dataTask(with: request) { data, _, error in
+            
+            guard let data = data, error == nil else {
+                return
+            }
+            
+            do{
+                let decoder = JSONDecoder()
+                let dataResponse = try decoder.decode(T.self, from: data)
+                onSuccess(dataResponse)
+            }catch {
+                onFail(error.localizedDescription)
+            }
+        }
+    }
+}
